f884b021e5b4337bb0518c715cd8183d3e2d03cc
Deogracious Mutyaba <ezramutyaba@gmail.com>
2022-08-30 12:13:30 +0100

    first post and get 1

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    username: str
+    password: str
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Bangui"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your post"}
+
+@app.post("/createposts")
+def create_posts(post:Post):
+    print(post.rating)
+    return {"data":post.dict()}
