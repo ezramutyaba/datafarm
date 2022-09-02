e7327065a078542ce60220425d8d200bb8d75967
Deogracious Mutyaba <ezramutyaba@gmail.com>
2022-09-02 11:06:46 +0100

    Restting Project

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Post(BaseModel):
-    username: str
-    password: str
-    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
